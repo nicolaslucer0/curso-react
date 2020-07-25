279bd38660dafc944d62963fa3bd99fa51f74929
nicolas.lucero <nicolas.lucero@redb.ee>
2020-07-25 16:48:58 -0300

    clase yanomeacuerdo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar/index.jsx";
-import Experiencia from "./components/Experiencia.jsx";
-import Contacto from "./components/Contacto.jsx";
-import Projects from "./components/Projects";
-import Personas from "./components/Personas.jsx";
+import Home from "./components/Home.jsx";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-
-      <Switch>
-        <Route path="/" exact>
-          <Experiencia titulo="tarejta copada" color="red" />
-        </Route>
-        <Route path="/personas">
-          <Personas />
-        </Route>
-        <Route path="/proyectos">
-          <Projects />
-        </Route>
-        <Route path="/contacto">
-          <Contacto />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <Home />;
 }
 
 export default App;
